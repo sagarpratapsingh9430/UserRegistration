a9420dae4fc7602989075adaab45c22f57fd51b6
Sagar Pratap Singh <sagarpratapsingh9432@gmail.com>
2023-07-06 16:56:36 +0530

    Percentage and grade claculate

--- src/com/bridgelabz/regix/UserRegistration.java
+++ src/com/bridgelabz/regix/UserRegistration.java
@@ -1,7 +1,23 @@
 package com.bridgelabz.regix;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
-    public static void main(String[] args) {
+    void validFirstName(){
+        Pattern pattern = Pattern.compile("[^A-Z][a-zA-Z][0-9]*$");
+        Pattern pattern1 = Pattern.compile("^[a-z]*.[a-z]+@[a-z]+.[a-z]{2,3}(.[a-z]{2})*$");
+        Pattern pattern2 = Pattern.compile("^[9][1]\s[6-9][0-9]{9}$");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first name: ");
+        String firstName = sc.nextLine();
+        Matcher matcher = pattern.matcher(firstName);
+        if (matcher.matches()){
+            System.out.println("First name is valid: ");
+        }else {
+            System.out.println("First name is not valid: ");
+        }
 
     }
 

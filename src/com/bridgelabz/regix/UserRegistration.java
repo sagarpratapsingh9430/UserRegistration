ac7fd17d25c73a3bcf22a76cf6cc430184a172b4
Sagar Pratap Singh <sagarpratapsingh9432@gmail.com>
2023-07-07 11:19:57 +0530

    As a User need to enter a valid Email Id

--- src/com/bridgelabz/regix/UserRegistration.java
+++ src/com/bridgelabz/regix/UserRegistration.java
@@ -8,7 +8,7 @@ public class UserRegistration {
     void validFirstName(){
         Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the first name: ");
+        System.out.print("Enter the first name: ");
         String firstName = sc.nextLine();
         Matcher matcher = pattern.matcher(firstName);
         if (matcher.matches()){
@@ -20,7 +20,8 @@ public class UserRegistration {
     void validLastName(){
         Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the last name: ");
+        System.out.println();
+        System.out.print("Enter the last name: ");
         String lastName = sc.nextLine();
         Matcher matcher = pattern.matcher(lastName);
         if (matcher.matches()){
@@ -29,11 +30,25 @@ public class UserRegistration {
             System.out.print("Not valid: ");
         }
     }
+    void validEmail(){
+        Pattern pattern = Pattern.compile("[a-z]+([+-._]{0,1}[a-z0-9])*@[a-z0-9]+[.][a-z]{2,3}([.]{0,1}[a-z]{2,4})*");
+        Scanner sc = new Scanner(System.in);
+        System.out.println();
+        System.out.print("Enter the email id: ");
+        String emailId = sc.nextLine();
+        Matcher matcher = pattern.matcher(emailId);
+        if (matcher.matches()){
+            System.out.print("Valid email id: " + emailId);
+        }else {
+            System.out.print("Not valid: ");
+        }
+    }
     public static void main(String[] args) {
         UserRegistration obj = new UserRegistration();
-        
+
         obj.validFirstName();
         obj.validLastName();
+        obj.validEmail();
 
 
     }

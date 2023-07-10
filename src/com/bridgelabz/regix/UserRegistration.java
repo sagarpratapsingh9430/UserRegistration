fea4065d99b8a5b2f04713ef63dc86af812cc6b7
Sagar Pratap Singh <sagarpratapsingh9432@gmail.com>
2023-07-10 22:57:33 +0530

    As a User need to follow pre-defined Password rules.           Rule1 – minimum 8 Characters

--- src/com/bridgelabz/regix/UserRegistration.java
+++ src/com/bridgelabz/regix/UserRegistration.java
@@ -5,54 +5,15 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-    void validFirstName(){
-        Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the first name: ");
-        String firstName = sc.nextLine();
-        Matcher matcher = pattern.matcher(firstName);
-        if (matcher.matches()){
-            System.out.print("Valid first name: " + firstName);
-        }else {
-            System.out.print("Not valid: ");
-        }
-    }
-    void validLastName(){
-        Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
+    void validPassword(){
+        Pattern pattern = Pattern.compile("^[A-Za-z]{8,}$");
         Scanner sc = new Scanner(System.in);
         System.out.println();
-        System.out.print("Enter the last name: ");
-        String lastName = sc.nextLine();
-        Matcher matcher = pattern.matcher(lastName);
+        System.out.print("Enter password: ");
+        String password = sc.nextLine();
+        Matcher matcher = pattern.matcher(password);
         if (matcher.matches()){
-            System.out.print("Valid last name: " + lastName);
-        }else {
-            System.out.print("Not valid: ");
-        }
-    }
-    void validEmail(){
-        Pattern pattern = Pattern.compile("[a-z]+([+-._]{0,1}[a-z0-9])*@[a-z0-9]+[.][a-z]{2,3}([.]{0,1}[a-z]{2,4})*");
-        Scanner sc = new Scanner(System.in);
-        System.out.println();
-        System.out.print("Enter the email id: ");
-        String emailId = sc.nextLine();
-        Matcher matcher = pattern.matcher(emailId);
-        if (matcher.matches()){
-            System.out.print("Valid email id: " + emailId);
-        }else {
-            System.out.print("Not valid: ");
-        }
-    }
-
-    void validMobileNumber(){
-        Pattern pattern = Pattern.compile("^[91]{2}\s[1-9]{9,}[0-9]+$");
-        Scanner sc = new Scanner(System.in);
-        System.out.println();
-        System.out.print("Enter mobile number: ");
-        String mobileNumber = sc.nextLine();
-        Matcher matcher = pattern.matcher(mobileNumber);
-        if (matcher.matches()){
-            System.out.print("Valid mobile number: " + mobileNumber);
+            System.out.print("Password is valid: " + password);
         }else {
             System.out.print("Not valid: ");
         }
@@ -60,10 +21,7 @@ public class UserRegistration {
     public static void main(String[] args) {
         UserRegistration obj = new UserRegistration();
 
-        obj.validFirstName();
-        obj.validLastName();
-        obj.validEmail();
-        obj.validMobileNumber();
+        obj.validPassword();
 
     }
 
